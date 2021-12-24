a4558f64b7d871149b584373c95600d0f1fd2faa
Andres <andrestheran@outlook.com>
2021-12-24 13:37:48 -0500

    final final

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function About( ) {
   return(
-  <h1> 
-  Nombre
-  Descripción
-  Imagen
-  Lista de 3 cosas que hayas aprendido en este programa
-  Correo
-  Link a tu Github
-  </h1>
+  <div>  
+  
+  <p>Andres Felipe Theran</p>
+  <br></br>
+  <p>Full Stack Web Developer (Loading...)</p>
+  <br></br>
+  <p></p>
+  <br></br>
+  <p>
+    1.Trabajo en Equipo
+    <br></br>
+    2.Investigar correctamente
+    <br></br>
+    3.Mejorar JS
+    <br></br>
+ 
+  </p>
+  <br></br>
+  <p>Andrestheran@outlook.com</p>
+  <br></br>
+
+  <a href="https://github.com/Andrestheran"> Andrestheran</a> 
+ 
+  </div>
   )
   }
